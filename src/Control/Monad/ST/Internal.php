8a2c07d5fbfce9b9038433840c04584067ee0c97
0x1 <kevin.francart3@gmail.com>
2026-07-30 13:50:53 +0200

    FFI: remove currying for AOT backend

--- src/Control/Monad/ST/Internal.php
+++ src/Control/Monad/ST/Internal.php
@@ -1,77 +1,28 @@
 <?php
 
-$map_ = function($f, $a = null) use (&$map_) {
-    if (\func_num_args() < 2) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$map_) {
-
-            return $map_(...\array_merge($__args, $more));
-        };
-    }
+$map_ = function($f, $a) use (&$map_) {
     return function() use($f, $a) { return $f($a()); };
 };
-$bind_ = function($a, $f = null) use (&$bind_) {
-    if (\func_num_args() < 2) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$bind_) {
-
-            return $bind_(...\array_merge($__args, $more));
-        };
-    }
+$bind_ = function($a, $f) use (&$bind_) {
     return function() use($a, $f) { return $f($a())(); };
 };
 $pure_ = function($a) { return function() use($a) { return $a; }; };
 $new = function($val) { return function() use($val) { return (object)['value' => $val]; }; };
 $read = function($ref) { return function() use($ref) { return $ref->value; }; };
-$modifyImpl = function($f, $ref = null) use (&$modifyImpl) {
-    if (\func_num_args() < 2) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$modifyImpl) {
-
-            return $modifyImpl(...\array_merge($__args, $more));
-        };
-    }
+$modifyImpl = function($f, $ref) use (&$modifyImpl) {
     return function() use($f, $ref) { $t = $f($ref->value); $ref->value = $t->state; return $t->value; };
 };
-$write = function($val, $ref = null) use (&$write) {
-    if (\func_num_args() < 2) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$write) {
-
-            return $write(...\array_merge($__args, $more));
-        };
-    }
+$write = function($val, $ref) use (&$write) {
     return function() use($val, $ref) { $ref->value = $val; return $val; };
 };
 $run = function($f) { return $f(); };
-$while = function($f, $a = null) use (&$while) {
-    if (\func_num_args() < 2) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$while) {
-
-            return $while(...\array_merge($__args, $more));
-        };
-    }
+$while = function($f, $a) use (&$while) {
     return function() use($f, $a) { while ($f()) { $a(); } return null; };
 };
-$for = function($lo, $hi = null, $f = null) use (&$for) {
-    if (\func_num_args() < 3) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$for) {
-
-            return $for(...\array_merge($__args, $more));
-        };
-    }
+$for = function($lo, $hi, $f) use (&$for) {
     return function() use($lo, $hi, $f) { for ($i = $lo; $i < $hi; $i++) { $f($i)(); } return null; };
 };
-$foreach = function($as, $f = null) use (&$foreach) {
-    if (\func_num_args() < 2) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$foreach) {
-
-            return $foreach(...\array_merge($__args, $more));
-        };
-    }
+$foreach = function($as, $f) use (&$foreach) {
     return function() use($as, $f) { foreach ($as as $a) { $f($a)(); } return null; };
 };
 
--- src/Control/Monad/ST/Uncurried.php
+++ src/Control/Monad/ST/Uncurried.php
@@ -4,131 +4,61 @@ $mkSTFn1 = function($fn) use (&$mkSTFn1) {
     return function($x) use ($fn) { return $fn($x)(); };
 };
 $mkSTFn2 = function($fn) use (&$mkSTFn2) {
-    return function($a, $b) use ($fn) { return $fn($a)($b)(); };
+    return function($a, $b) use ($fn) { return $fn($a, $b)(); };
 };
 $mkSTFn3 = function($fn) use (&$mkSTFn3) {
-    return function($a, $b, $c) use ($fn) { return $fn($a)($b)($c)(); };
+    return function($a, $b, $c) use ($fn) { return $fn($a, $b, $c)(); };
 };
 $mkSTFn4 = function($fn) use (&$mkSTFn4) {
-    return function($a, $b, $c, $d) use ($fn) { return $fn($a)($b)($c)($d)(); };
+    return function($a, $b, $c, $d) use ($fn) { return $fn($a, $b, $c, $d)(); };
 };
 $mkSTFn5 = function($fn) use (&$mkSTFn5) {
-    return function($a, $b, $c, $d, $e) use ($fn) { return $fn($a)($b)($c)($d)($e)(); };
+    return function($a, $b, $c, $d, $e) use ($fn) { return $fn($a, $b, $c, $d, $e)(); };
 };
 $mkSTFn6 = function($fn) use (&$mkSTFn6) {
-    return function($a, $b, $c, $d, $e, $f) use ($fn) { return $fn($a)($b)($c)($d)($e)($f)(); };
+    return function($a, $b, $c, $d, $e, $f) use ($fn) { return $fn($a, $b, $c, $d, $e, $f)(); };
 };
 $mkSTFn7 = function($fn) use (&$mkSTFn7) {
-    return function($a, $b, $c, $d, $e, $f, $g) use ($fn) { return $fn($a)($b)($c)($d)($e)($f)($g)(); };
+    return function($a, $b, $c, $d, $e, $f, $g) use ($fn) { return $fn($a, $b, $c, $d, $e, $f, $g)(); };
 };
 $mkSTFn8 = function($fn) use (&$mkSTFn8) {
-    return function($a, $b, $c, $d, $e, $f, $g, $h) use ($fn) { return $fn($a)($b)($c)($d)($e)($f)($g)($h)(); };
+    return function($a, $b, $c, $d, $e, $f, $g, $h) use ($fn) { return $fn($a, $b, $c, $d, $e, $f, $g, $h)(); };
 };
 $mkSTFn9 = function($fn) use (&$mkSTFn9) {
-    return function($a, $b, $c, $d, $e, $f, $g, $h, $i) use ($fn) { return $fn($a)($b)($c)($d)($e)($f)($g)($h)($i)(); };
+    return function($a, $b, $c, $d, $e, $f, $g, $h, $i) use ($fn) { return $fn($a, $b, $c, $d, $e, $f, $g, $h, $i)(); };
 };
 $mkSTFn10 = function($fn) use (&$mkSTFn10) {
-    return function($a, $b, $c, $d, $e, $f, $g, $h, $i, $j) use ($fn) { return $fn($a)($b)($c)($d)($e)($f)($g)($h)($i)($j)(); };
+    return function($a, $b, $c, $d, $e, $f, $g, $h, $i, $j) use ($fn) { return $fn($a, $b, $c, $d, $e, $f, $g, $h, $i, $j)(); };
 };
 
-$runSTFn1 = function($fn, $a = null) use (&$runSTFn1) {
-    if (\func_num_args() < 2) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$runSTFn1) {
-
-            return $runSTFn1(...\array_merge($__args, $more));
-        };
-    }
+$runSTFn1 = function($fn, $a) use (&$runSTFn1) {
     return function() use ($fn, $a) { return $fn($a); };
 };
-$runSTFn2 = function($fn, $a = null, $b = null) use (&$runSTFn2) {
-    if (\func_num_args() < 3) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$runSTFn2) {
-
-            return $runSTFn2(...\array_merge($__args, $more));
-        };
-    }
+$runSTFn2 = function($fn, $a, $b) use (&$runSTFn2) {
     return function() use ($fn, $a, $b) { return $fn($a, $b); };
 };
-$runSTFn3 = function($fn, $a = null, $b = null, $c = null) use (&$runSTFn3) {
-    if (\func_num_args() < 4) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$runSTFn3) {
-
-            return $runSTFn3(...\array_merge($__args, $more));
-        };
-    }
+$runSTFn3 = function($fn, $a, $b, $c) use (&$runSTFn3) {
     return function() use ($fn, $a, $b, $c) { return $fn($a, $b, $c); };
 };
-$runSTFn4 = function($fn, $a = null, $b = null, $c = null, $d = null) use (&$runSTFn4) {
-    if (\func_num_args() < 5) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$runSTFn4) {
-
-            return $runSTFn4(...\array_merge($__args, $more));
-        };
-    }
+$runSTFn4 = function($fn, $a, $b, $c, $d) use (&$runSTFn4) {
     return function() use ($fn, $a, $b, $c, $d) { return $fn($a, $b, $c, $d); };
 };
-$runSTFn5 = function($fn, $a = null, $b = null, $c = null, $d = null, $e = null) use (&$runSTFn5) {
-    if (\func_num_args() < 6) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$runSTFn5) {
-
-            return $runSTFn5(...\array_merge($__args, $more));
-        };
-    }
+$runSTFn5 = function($fn, $a, $b, $c, $d, $e) use (&$runSTFn5) {
     return function() use ($fn, $a, $b, $c, $d, $e) { return $fn($a, $b, $c, $d, $e); };
 };
-$runSTFn6 = function($fn, $a = null, $b = null, $c = null, $d = null, $e = null, $f = null) use (&$runSTFn6) {
-    if (\func_num_args() < 7) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$runSTFn6) {
-
-            return $runSTFn6(...\array_merge($__args, $more));
-        };
-    }
+$runSTFn6 = function($fn, $a, $b, $c, $d, $e, $f) use (&$runSTFn6) {
     return function() use ($fn, $a, $b, $c, $d, $e, $f) { return $fn($a, $b, $c, $d, $e, $f); };
 };
-$runSTFn7 = function($fn, $a = null, $b = null, $c = null, $d = null, $e = null, $f = null, $g = null) use (&$runSTFn7) {
-    if (\func_num_args() < 8) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$runSTFn7) {
-
-            return $runSTFn7(...\array_merge($__args, $more));
-        };
-    }
+$runSTFn7 = function($fn, $a, $b, $c, $d, $e, $f, $g) use (&$runSTFn7) {
     return function() use ($fn, $a, $b, $c, $d, $e, $f, $g) { return $fn($a, $b, $c, $d, $e, $f, $g); };
 };
-$runSTFn8 = function($fn, $a = null, $b = null, $c = null, $d = null, $e = null, $f = null, $g = null, $h = null) use (&$runSTFn8) {
-    if (\func_num_args() < 9) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$runSTFn8) {
-
-            return $runSTFn8(...\array_merge($__args, $more));
-        };
-    }
+$runSTFn8 = function($fn, $a, $b, $c, $d, $e, $f, $g, $h) use (&$runSTFn8) {
     return function() use ($fn, $a, $b, $c, $d, $e, $f, $g, $h) { return $fn($a, $b, $c, $d, $e, $f, $g, $h); };
 };
-$runSTFn9 = function($fn, $a = null, $b = null, $c = null, $d = null, $e = null, $f = null, $g = null, $h = null, $i = null) use (&$runSTFn9) {
-    if (\func_num_args() < 10) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$runSTFn9) {
-
-            return $runSTFn9(...\array_merge($__args, $more));
-        };
-    }
+$runSTFn9 = function($fn, $a, $b, $c, $d, $e, $f, $g, $h, $i) use (&$runSTFn9) {
     return function() use ($fn, $a, $b, $c, $d, $e, $f, $g, $h, $i) { return $fn($a, $b, $c, $d, $e, $f, $g, $h, $i); };
 };
-$runSTFn10 = function($fn, $a = null, $b = null, $c = null, $d = null, $e = null, $f = null, $g = null, $h = null, $i = null, $j = null) use (&$runSTFn10) {
-    if (\func_num_args() < 11) {
-        $__args = \func_get_args();
-        return function(...$more) use ($__args, &$runSTFn10) {
-
-            return $runSTFn10(...\array_merge($__args, $more));
-        };
-    }
+$runSTFn10 = function($fn, $a, $b, $c, $d, $e, $f, $g, $h, $i, $j) use (&$runSTFn10) {
     return function() use ($fn, $a, $b, $c, $d, $e, $f, $g, $h, $i, $j) { return $fn($a, $b, $c, $d, $e, $f, $g, $h, $i, $j); };
 };
 
